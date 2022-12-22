c807d09f6483e68d1a1bdab123e603ab818cb98c
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2022-12-22 22:10:41 +0700

    'countries'

--- src/components/Highlight/index.js
+++ src/components/Highlight/index.js
@@ -1,8 +1,40 @@
+import { Card, CardContent, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 const Highlight = () => {
   return (
-    <div>Highlight</div>
+    <>
+        <Grid container spacing={3}>
+
+            <Grid item sm={4} xs={12}>
+                <Card>
+                    <CardContent>
+                        <Typography component="p" variant="body2">Số Ca Mắc</Typography>
+                        <Typography component="span" variant="body2">3000</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid item sm={4} xs={12}>
+                <Card>
+                    <CardContent>
+                        <Typography component="p" variant="body2">Số Ca Khỏi</Typography>
+                        <Typography component="span" variant="body2">3000</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid item sm={4} xs={12}>
+                <Card>
+                    <CardContent>
+                        <Typography component="p" variant="body2">Số Ca Tử Vong</Typography>
+                        <Typography component="span" variant="body2">3000</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+        </Grid>
+    </>
   )
 }
 
--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -1,9 +1,20 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 const Summary = () => {
-  return (
-    <div>Summary</div>
-  )
+    return (
+        <div>
+            <Grid container spacing={3}>
+                <Grid item sm={8} xs={12}>
+
+                </Grid>
+                <Grid item sm={4} xs={12}>
+
+                </Grid>
+                
+            </Grid>
+        </div>
+    )
 }
 
 export default Summary
